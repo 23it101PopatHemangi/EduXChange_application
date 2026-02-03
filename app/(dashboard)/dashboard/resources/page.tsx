82e1ce1690cc9ae598c573410e970643f78bb772
unknown <23it101@charusat.edu.in>
2026-02-03 05:42:52 +0530

    Updated DeleteResourceButton to include resourceTitle prop

--- app/(dashboard)/dashboard/resources/page.tsx
+++ app/(dashboard)/dashboard/resources/page.tsx
@@ -154,7 +154,7 @@ export default async function ResourcesPage({ searchParams }: { searchParams: { 
                         </DropdownMenuItem>
                         <DropdownMenuSeparator />
                         <DropdownMenuItem asChild>
-                          <DeleteResourceButton resourceId={resource.id} resourceTitle={resource.name || 'No title available'} />
+                          <DeleteResourceButton resourceId={resource.id} resourceTitle={resource.name || 'Untitled Resource'} />
                         </DropdownMenuItem>
                       </DropdownMenuContent>
                     </DropdownMenu>
